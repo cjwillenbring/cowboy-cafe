f8da8a28057eefa08f99420865cf8cf2009082bf
Cole W <cole@willenbrings.com>
2020-01-22 16:09:56 -0600

    Added class definitions for the 3 burger entrees and the 2 sandwich entrees

---
+++ Data/AngryChicken.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Angry Chicken Sandwich entree
+    /// </summary>
+    class AngryChicken
+    {
+        private bool bread = true;
+        /// <summary>
+        /// If the pulled pork is accompanied with bread (sandwich)
+        /// </summary>
+        public bool Bread
+        {
+            get { return bread; }
+            set { bread = value; }
+        }
+
+        private bool pickle = true;
+        /// <summary>
+        /// If the sandwich comes with a pickle
+        /// </summary>
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        /// <summary>
+        /// The price of the sandwich
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 5.99;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the sandwich
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 190;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the sandwich
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bread) instructions.Add("hold bread");
+                if (!pickle) instructions.Add("hold pickle");
+
+                return instructions;
+            }
+        }
+    }
+}
