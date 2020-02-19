f4b798962b5f11ec319be9ac342fe6026872375f
Cole Willenbring <cjwillenbring@users.campus>
2020-02-19 14:40:09 -0600

    Added event handlers and documentation to the POS app

--- PointOfSale/OrderControl.xaml.cs
+++ PointOfSale/OrderControl.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CowboyCafe.Data;
 
 namespace PointOfSale
 {
@@ -18,9 +19,177 @@ namespace PointOfSale
     /// </summary>
     public partial class OrderControl : UserControl
     {
+        /// <summary>
+        /// Initialize the Order Control class and add event handlers
+        /// </summary>
         public OrderControl()
         {
             InitializeComponent();
+            AddCowpokeChiliButton.Click += OnAddCowpokeChiliButtonClicked;
+            AddRustlersRibsButton.Click += OnAddRustlersRibsButtonClicked;
+            AddPecosPulledPorkButton.Click += OnAddPecosPulledPorkButtonClicked;
+            AddTrailBurgerButton.Click += OnAddTrailBurgerButtonClicked; 
+            AddDakotaDoubleBurgerButton.Click += OnAddDakotaDoubleBurgerButtonClicked; 
+            AddTexasTripleBurgerButton.Click += OnAddTexasTripleBurgerButtonClicked; 
+            AddAngryChickenButton.Click += OnAddAngryChickenButtonClicked; 
+            AddChiliCheeseFriesButton.Click += OnAddChiliCheeseFriesButtonClicked; 
+            AddCornDodgersButton.Click += OnAddCornDodgersButtonClicked; 
+            AddPandeCampoButton.Click += OnAddPandeCampoButtonClicked; 
+            AddBakedBeansButton.Click += OnAddBakedBeansButtonClicked;
+            AddJerkedSodaButton.Click += OnAddJerkedSodaButtonClicked;
+            AddTexasTeaButton.Click += OnAddTexasTeaButtonClicked;
+            AddCowboyCoffeeButton.Click += OnAddCowboyCoffeeButtonClicked;
+            AddWaterButton.Click += OnAddWaterButtonClicked;
+        }
+
+        /// <summary>
+        /// Event handler for adding the cowpoke chili entree to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddCowpokeChiliButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new CowpokeChili());
+        }
+
+        /// <summary>
+        /// Event handler for adding the rustler's ribs entree to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddRustlersRibsButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new RustlersRibs());
+        }
+
+        /// <summary>
+        /// Event handler for adding the pulled pork entree to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddPecosPulledPorkButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new PecosPulledPork());
+        }
+
+        /// <summary>
+        /// Event handler for adding the trail burger entree to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddTrailBurgerButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new TrailBurger());
+        }
+
+        /// <summary>
+        /// Event handler for adding the dakota double burger entree to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddDakotaDoubleBurgerButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new DakotaDoubleBurger());
+        }
+
+        /// <summary>
+        /// Event handler for adding the texas triple burger entree to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddTexasTripleBurgerButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new TexasTripleBurger());
+        }
+
+        /// <summary>
+        /// Event handler for adding the angry chicken entree to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddAngryChickenButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new AngryChicken());
+        }
+
+        /// <summary>
+        /// Event handler for adding the chili cheese fries side to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddChiliCheeseFriesButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new ChiliCheeseFries());
+        }
+
+        /// <summary>
+        /// Event handler for adding the corn dodgers side to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddCornDodgersButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new CornDodgers());
+        }
+
+        /// <summary>
+        /// Event handler for adding the pan de campo side to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddPandeCampoButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new PanDeCampo());
+        }
+
+        /// <summary>
+        /// Event handler for adding the baked beans side to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddBakedBeansButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new BakedBeans());
+        }
+
+        /// <summary>
+        /// Event handler for adding the jerked soda drink to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddJerkedSodaButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new JerkedSoda());
+        }
+
+        /// <summary>
+        /// Event handler for adding the Texas Tea drink to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddTexasTeaButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new TexasTea());
+        }
+
+        /// <summary>
+        /// Event handler for adding the cowboy coffee drink to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddCowboyCoffeeButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new CowboyCoffee());
+        }
+
+        /// <summary>
+        /// Event handler for adding the water drink to the listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddWaterButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new Water());
         }
     }
 }
